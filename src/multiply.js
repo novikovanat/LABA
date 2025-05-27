81311f9402f4d2fc426a27c4b41c49fa76475918
novikovanat <72397136+novikovanat@users.noreply.github.com>
2025-05-27 18:44:55 +0200

    first

---
+++ src/multiply.js
@@ -0,0 +1,4 @@
+const string1 = "5";
+const string2 = "5";
+const string3 = string1 * string2;
+console.log(string3);
---
+++ src/plus.js
@@ -0,0 +1,4 @@
+const string1 = "16";
+const string2 = "5";
+const string3 = string1 + string2;
+console.log(string3);
