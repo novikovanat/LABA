76eff7cf77c0bab66a6fd65d90bdda0af64d13f4
novikovanat <72397136+novikovanat@users.noreply.github.com>
2025-05-29 14:27:16 +0200

    plus changed

--- src/math.js
+++ src/math.js
@@ -1,24 +1,45 @@
-const string1 =
-  "999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999";
-const string2 =
-  "999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999";
+const string1 = "118";
+const string2 = "8";
 
-Object.assign(String.prototype, { multiply, plus, minus, divide });
+Object.assign(String.prototype, { multiply, minus, divide, plus });
 
 console.log(string1.multiply(string2));
-console.log(string1.plus(string2));
 console.log(string1.minus(string2));
 console.log(string1.divide(string2));
-console.log(String.prototype);
+console.log(string1.plus(string2));
 
 function multiply(arg) {
-  const result = parseInt(this) * parseInt(arg);
+  const result = Number.parseInt(this) * Number.parseInt(arg);
   return result.toString();
 }
 
 function plus(arg) {
-  const result = parseInt(this) + parseInt(arg);
-  return result.toString();
+  let res = "";
+  let carry = 0;
+
+  let firstStringArray = this.split("").reverse();
+  let secondStringArray = arg.split("").reverse();
+
+  let arrRest = firstStringArray.length - secondStringArray.length;
+
+  if (arrRest < 0) {
+    arrRest = arrRest * -1;
+    firstStringArray = firstStringArray.concat(new Array(arrRest).fill(0));
+  } else {
+    secondStringArray = secondStringArray.concat(new Array(arrRest).fill(0));
+  }
+
+  for (let i = 0; i < firstStringArray.length; i++) {
+    let digitA = parseInt(firstStringArray[i]);
+    let digitB = parseInt(secondStringArray[i]);
+
+    let sum = digitA + digitB + carry;
+    res = (sum % 10) + res;
+    carry = Math.floor(sum / 10);
+  }
+
+  if (carry) res = carry + res;
+  return res.replace(/^0+/, "") || "0";
 }
 
 function minus(arg) {
